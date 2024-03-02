f468c59bcc8151eb7d3abd12ada28e2dcb01b7cd
CultOfHeisenberg <renee.tan2@student.education.wa.edu.au>
2024-03-02 14:16:46 +0800

    Created a nested dictionary to allow easy conversion between time periods as per part 1.2 of the brief. Put one dictionary of DAYS for week, month, quarter, year etc. and one for WEEKS to address most possible interest compounding periods

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("grah")
+# defining time periods in days
+time_dictionary = {
+    "days_dict": {
+        "week": 7,
+        "month": 30,
+        "quarter": 90,  # 3 months times 30 days
+        "year": 365
+    },
+    "week_dict": {
+        "week": 1,
+        "month": 4,
+        "quarter": 12,
+        "year": 52
+    }
+}
