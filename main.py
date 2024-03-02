ddbe679170236480d9eba3582581d3f19123347d
CultOfHeisenberg <renee.tan2@student.education.wa.edu.au>
2024-03-02 14:26:26 +0800

    initialising simple interest variables by assigning None. will append later with user input

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ time_dictionary = {
         "year": 52
     }
 }
+
+## simple interest formula, A = P(1+rt)
+## P is the principal
+## r = interest rate PER YEAR as a decimal
+## t = time in YEARS elapsed
+## simple interest WITH principal
+
+# keeping variables empty for later appending by assigning None
+principal = None
+interest_rate = None
+time = None
+total_amount = None
+
