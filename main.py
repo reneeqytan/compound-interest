8190bef3da94b75ed053878d2f52b66af3dd9469
CultOfHeisenberg <renee.tan2@student.education.wa.edu.au>
2024-03-02 15:37:42 +0800

    started the total amount for simple interest calculations. added a rounding feature for cents. all parts of formula done except the time elapsed

--- main.py
+++ main.py
@@ -22,17 +22,27 @@ time_dictionary = {
 
 # keeping variables empty for later appending by assigning None
 principal = None
-interest_rate = None
-time = None
+interest_rate_percentage = None
+time_unit = None # how often interest is added e.g. year, week, etc.
+time_elapsed = None # how much time passes in the units of time_unit, e.g. 4, 5
+time_period = None # time elapsed and time unit multiplied, provides a final time e.g. 4 years, 5 month etc.
 total_amount = None
+
 
 ## SIMPLE INTEREST VS COMPOUND COMPARISON
 
 print("Simple Interest Account")
 principal = (int(input("Enter the principal amount in $: ")))
-interest_rate = (int(input("Enter the interest rate without the percentage sign: ")))
-time = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
+interest_rate_percentage = (int(input("Enter the interest rate without the percentage sign: ")))
+time_unit = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
+time_elapsed = int(input("Enter the number of the previous time unit the interest will be calculated for: "))
 
-# calculate simple interest
-total_amount = principal*(1 + interest_rate)
+if time_unit == "year":
+    time_period = time_dictionary["days_dict"]["year"] * time_elapsed
+
+interest_rate_decimal = interest_rate_percentage / 100 # converting the interest rate % to a decimal by dividing 100
+
+
+# calculate simple interest, following formula of A = P(1 + rt)
+total_amount = round(principal*(1 + interest_rate_decimal), 2) # rounding to 2 dec places cause cents
 print(total_amount)
