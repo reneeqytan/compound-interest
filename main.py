861cd649254336098e78f3edfd761f16ac7cf4d2
CultOfHeisenberg <renee.tan2@student.education.wa.edu.au>
2024-03-03 18:22:14 +0800

    minor fixes regarding the time unit conversions to a time period, finalised the formula

--- main.py
+++ main.py
@@ -23,11 +23,10 @@ time_dictionary = {
 # keeping variables empty for later appending by assigning None
 principal = None
 interest_rate_percentage = None
-time_unit = None # how often interest is added e.g. year, week, etc.
-time_elapsed = None # how much time passes in the units of time_unit, e.g. 4, 5
-time_period = None # time elapsed and time unit multiplied, provides a final time e.g. 4 years, 5 month etc.
+time_unit = None  # how often interest is added e.g. year, week, etc.
+time_elapsed = None  # how much time passes in the units of time_unit, e.g. 4, 5
+time_period = None  # time elapsed and time unit multiplied, provides a final time e.g. 4 years, 5 month etc.
 total_amount = None
-
 
 ## SIMPLE INTEREST VS COMPOUND COMPARISON
 
@@ -37,11 +36,12 @@ interest_rate_percentage = (int(input("Enter the interest rate without the perce
 time_unit = input("Enter the interest rate time unit (year, quarter, month, week, day): ")
 time_elapsed = int(input("Enter the number of the previous time unit the interest will be calculated for: "))
 
-if time_unit == "year":
-    time_period = time_dictionary["days_dict"]["year"] * time_elapsed
 
-interest_rate_decimal = interest_rate_percentage / 100 # converting the interest rate % to a decimal by dividing 100
+if time_unit == "year":
+    time_period = 1 * time_elapsed # the number of years elapsed calculation to get the final time period
+
+interest_rate_decimal = interest_rate_percentage / 100  # converting the interest rate % to a decimal by dividing 100
 
 # calculate simple interest, following formula of A = P(1 + rt)
-total_amount = round(principal*(1 + interest_rate_decimal), 2) # rounding to 2 dec places cause cents
+total_amount = round(principal * (1 + interest_rate_decimal * time_period), 2)  # rounding to 2 dec places cause cents
 print(total_amount)
